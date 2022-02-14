643ec1dd1dc81ef3155105a143d10ae64c93b1ee
nontangent <irregular0number@gmail.com>
2022-02-14 21:53:07 +0900

    feat(common): enable to inject transformer of `SmartExpPipe`

--- libs/common/pipes/smart-exp/smart-exp.pipe.ts
+++ libs/common/pipes/smart-exp/smart-exp.pipe.ts
@@ -1,20 +1,31 @@
-import { Pipe } from '@angular/core';
+import { Inject, InjectionToken, Optional, Pipe } from '@angular/core';
 import * as dayjs from 'dayjs';
 import { YenPipe } from '../yen/yen.pipe';
+
+export const SMART_EXP_TRANSFORMER = new InjectionToken<SmartExpTransformer>('[@ng-atomic/common] Smart Transformer');
+
+export type SmartExpTransformer = (input: string | number | dayjs.Dayjs | any, key?: string) => string;
+
+export const smartExpTransformer: SmartExpTransformer = input => dayjs.isDayjs(input)
+  ? input.format('YY/MM/DD')
+  : Array.isArray(input) 
+  ? `${input.length}個のアイテム`
+  : typeof input === 'number' && input > 10_000
+  ? new YenPipe().transform(input)
+  : input;
 
 @Pipe({
   name: 'smartExp',
   pure: true,
 })
 export class SmartExpPipe {
+  constructor(
+    @Optional() @Inject(SMART_EXP_TRANSFORMER) private transformer: SmartExpTransformer
+  ) {
+    this.transformer ??= smartExpTransformer;
+  }
+
   transform(input: string | number | dayjs.Dayjs | any, key?: string): string {
-    if (dayjs.isDayjs(input)) {
-      return input.format('YY/MM/DD');
-    } else if (Array.isArray(input)) {
-      return `${input.length}個のアイテム`;
-    } else if (typeof input === 'number' && input > 10_000) {
-      return new YenPipe().transform(input);
-    }
-    return input;
+    return this.transformer(input, key);
   }
 }
