5b1efff641f8b503ea343aca1981b4354f9bdf88
nontangent <irregular0number@gmail.com>
2022-02-20 00:21:46 +0900

    feat(storybook): add build config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
   "stories": [
+    "../docs/**/*.stories.mdx",
     "../libs/components/**/*.stories.mdx",
     "../libs/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
   "framework": "@storybook/angular"
 }
---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+export const parameters = {
+  options: {
+    storySort: {
+      order: [
+        'Introduction',
+        'Introduction/Welcome', 
+        'Introduction/Schematics',
+        'Atoms', 'Molecules', 'Organisms', 'Templates', 'Pages', 'Frames'],
+    },
+  },
+};
