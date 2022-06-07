2e7852f5f3f5bbd723d13dce209b635e2188e1f4
nontangent <irregular0number@gmail.com>
2022-06-08 00:38:31 +0900

    feat(templates): use text input section

--- libs/components/templates/entrance/entrance.module.ts
+++ libs/components/templates/entrance/entrance.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionButtonsSectionModule } from '@ng-atomic/components/organisms/action-buttons-section';
 import { HeadingModule } from '@ng-atomic/components/organisms/heading';
-import { InputFieldSectionModule } from '@ng-atomic/components/organisms/input-field-section';
+import { TextInputSectionModule } from '@ng-atomic/components/organisms/text-input-section';
 import { SocialLoginSectionModule } from '@ng-atomic/components/organisms/social-login-section';
 import { AutoLayoutModule } from '@ng-atomic/components/frames/auto-layout';
 import { CardModule } from '@ng-atomic/components/frames/card';
@@ -21,8 +21,8 @@ import { EntranceTemplate } from './entrance.template';
     // Organisms
     ActionButtonsSectionModule,
     HeadingModule,
-    InputFieldSectionModule,
     SocialLoginSectionModule,
+    TextInputSectionModule,
   ],
   exports: [
     EntranceTemplate
