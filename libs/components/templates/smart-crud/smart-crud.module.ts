6305ace3f6f01ddafe6f4c1a7be9db77b57ad8ef
nontangent <irregular0number@gmail.com>
2022-06-08 00:39:07 +0900

    feat(templates): use text input section

--- libs/components/templates/smart-crud/smart-crud.module.ts
+++ libs/components/templates/smart-crud/smart-crud.module.ts
@@ -5,7 +5,7 @@ import { ScrollModule } from '@ng-atomic/components/frames/scroll';
 import { AutoLayoutModule } from '@ng-atomic/components/frames/auto-layout';
 import { ActionButtonsSectionModule } from '@ng-atomic/components/organisms/action-buttons-section';
 import { BackNavigatorModule } from '@ng-atomic/components/organisms/back-navigator';
-import { InputFieldSectionModule } from '@ng-atomic/components/organisms/input-field-section';
+import { TextInputSectionModule } from '@ng-atomic/components/organisms/text-input-section';
 import { DateInputSectionModule } from '@ng-atomic/components/organisms/date-input-section';
 import { SelectInputSectionModule } from '@ng-atomic/components/organisms/select-input-section';
 import { SmartFieldModule } from '@ng-atomic/common/pipes';
@@ -31,7 +31,7 @@ import { SmartCrudTemplate } from './smart-crud.template';
     ActionButtonsSectionModule,
     BackNavigatorModule,
     DateInputSectionModule,
-    InputFieldSectionModule,
+    TextInputSectionModule,
     SelectInputSectionModule,
   ],
   exports: [
