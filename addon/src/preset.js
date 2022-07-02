130b6f25987e12793156baf1abc061d7a1c68410
nontangent <irregular0number@gmail.com>
2022-07-03 01:38:46 +0900

    refactor: clean code

--- addon/src/preset.js
+++
@@ -1,7 +0,0 @@
-const { resolve } = require('path');
-
-function managerEntries(entry = []) {
-  return [...entry, require.resolve(resolve(__dirname, './register.tsx'))];
-}
-
-module.exports = { managerEntries };
