0a49acfb58dcb042e2201c2773957fbaffcd09b5
nontangent <irregular0number@gmail.com>
2022-07-24 04:00:00 +0900

    feat(common): remove defineElements function

--- libs/common/src/utils/define-element.ts
+++
@@ -1,8 +0,0 @@
-import { Injector } from '@angular/core';
-
-export async function defineElement(injector: Injector, Component: any) {
-  return import('@angular/elements').then(({createCustomElement}) => {
-    const el = createCustomElement(Component, { injector });
-    customElements.define('pages-blank', el);
-  });
-}
