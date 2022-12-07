e10c86a38a99c92467b0bc83bb1e227531d61fe3
nontangent <irregular0number@gmail.com>
2022-12-07 21:02:10 +0900

    feat(schematics-x): export schematics cli main

--- libs/schematics-x/src/lib/bin/schematics-x.ts
+++ libs/schematics-x/src/lib/bin/schematics-x.ts
@@ -9,50 +9,55 @@ import { CliOptions, CLI_OPTIONS_KEY } from './parse-cli-options';
 const COLLECTION_JSON_PATH = resolve(__dirname, '../../../collection.json');
 const COLLECTION = process.env['DEBUG'] ? COLLECTION_JSON_PATH : 'schematics-x';
 
-const program = new Command();
-program.version(packageJson.version, '-v, --version', 'output the current version');
-
-for (const [name, schematic] of Object.entries(collectionJson.schematics)) {
-  const command = program.command(name).description(schematic.description);
-  const { properties } = require(resolve(COLLECTION_JSON_PATH, '../', schematic.schema));
-
-  Object.entries<any>(properties).forEach(([key, { type, description, default: defaultValue }]) => {
-    const flags = type === 'boolean' ? `--${key} <${type}>` : `--${key} <${type}>`;
-    command.option(flags, description, defaultValue);
-  });
-
-  command.option(`--interactive`, ``, true);
-  command.option(`--force`, ``, false);
-  command.option(`--verbose`, ``, false);
-  command.option(`--allow-private`, ``, false);
-  command.option(`--debug`, ``,  null);
-  command.option(`--dry-run`, ``, null);
-
-  command
-    .arguments('[args...]')
-    .allowUnknownOption()
-    .action((schematicArgs, options) => {
-      const cliOptions: CliOptions = {};
-      const schematicOptions: Record<string, unknown> = {};
-
-      Object.entries(options).forEach(([key, value]) => {
-        if (CLI_OPTIONS_KEY.includes(key as any)) {
-          cliOptions[key] = value;
-        } else {
-          schematicOptions[key] = value === 'true' ? true : value === 'false' ? false : value;
-        }
-      });
-      
-      runWorkflow({
-        cliOptions,
-        collectionName: COLLECTION,
-        schematicName: name,
-        schematicOptions,
-        schematicArgs,
-      })
-        .then((exitCode) => (process.exitCode = exitCode))
-        .catch((e) => { throw e; });
+export async function main() {
+  const program = new Command();
+  program.version(packageJson.version, '-v, --version', 'output the current version');
+  
+  for (const [name, schematic] of Object.entries(collectionJson.schematics)) {
+    const command = program.command(name).description(schematic.description);
+    const { properties } = require(resolve(COLLECTION_JSON_PATH, '../', schematic.schema));
+  
+    Object.entries<any>(properties).forEach(([key, { type, description, default: defaultValue }]) => {
+      const flags = type === 'boolean' ? `--${key} <${type}>` : `--${key} <${type}>`;
+      command.option(flags, description, defaultValue);
     });
+  
+    command.option(`--interactive`, ``, true);
+    command.option(`--force`, ``, false);
+    command.option(`--verbose`, ``, false);
+    command.option(`--allow-private`, ``, false);
+    command.option(`--debug`, ``,  null);
+    command.option(`--dry-run`, ``, null);
+  
+    command
+      .arguments('[args...]')
+      .allowUnknownOption()
+      .action((schematicArgs, options) => {
+        const cliOptions: CliOptions = {};
+        const schematicOptions: Record<string, unknown> = {};
+  
+        Object.entries(options).forEach(([key, value]) => {
+          if (CLI_OPTIONS_KEY.includes(key as any)) {
+            cliOptions[key] = value;
+          } else {
+            schematicOptions[key] = value === 'true' ? true : value === 'false' ? false : value;
+          }
+        });
+        
+        runWorkflow({
+          cliOptions,
+          collectionName: COLLECTION,
+          schematicName: name,
+          schematicOptions,
+          schematicArgs,
+        })
+          .then((exitCode) => (process.exitCode = exitCode))
+          .catch((e) => { throw e; });
+      });
+  }
+  program.parse();
 }
 
-program.parse();
+if (require.main === module) {
+  main();
+}
