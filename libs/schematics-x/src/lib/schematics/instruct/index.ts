3372b080f61e36fa7237c5a2d23c0410991eceff
nontangent <irregular0number@gmail.com>
2022-12-07 23:54:41 +0900

    feat(schematics-x): add `scope` options for setting `inputScope` and `outputScope` at the same time

--- libs/schematics-x/src/lib/schematics/instruct/index.ts
+++ libs/schematics-x/src/lib/schematics/instruct/index.ts
@@ -6,9 +6,10 @@ import { tryResolveBasePath, updateTree } from '../utils';
 
 export interface InstructSchema extends BaseSchema {
   instructions: string;
-  inputScope: string;
-  outputScope: string;
+  scope: string;
 
+  inputScope?: string;
+  outputScope?: string;
   inputs?: string;
   outputs?: string;
 }
@@ -17,8 +18,8 @@ export const instruct = (options: InstructSchema): Rule => async (tree: Tree) =>
 	const projectBasePath = await tryResolveBasePath(tree, options.project, options.path);
   const schematicsX = new SchematicsX();
   const entries = await schematicsX.execute(tree, {
-    inputScope: join(projectBasePath, options.inputScope),
-    outputScope: join(projectBasePath, options.outputScope),
+    inputScope: join(projectBasePath, options.inputScope ?? options.scope),
+    outputScope: join(projectBasePath, options.outputScope ?? options.scope),
     instructions: options.instructions,
     inputFilePaths: options.inputs?.split(',').map(filePath => join(projectBasePath, filePath)),
     outputFilePaths: options.outputs?.split(',').map(filePath => join(projectBasePath, filePath)),
