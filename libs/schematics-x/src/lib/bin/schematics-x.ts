1d1c8461847ba4b1c426bcf36aec5950b722ad80
nontangent <irregular0number@gmail.com>
2022-12-10 23:26:25 +0900

    feat(schematics-x): add feature of detecting project from `angular.json`

--- libs/schematics-x/src/lib/bin/schematics-x.ts
+++ libs/schematics-x/src/lib/bin/schematics-x.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 import collectionJson from '../../../collection.json';
 import packageJson from '../../../package.json';
 import { CliOptions, CLI_OPTIONS_KEY } from './parse-cli-options';
+import { getProjectByCwd, getWorkspace } from '@angular/cli/src/utilities/config';
 
 const COLLECTION_JSON_PATH = resolve(__dirname, '../../../collection.json');
 const COLLECTION = process.env['DEBUG'] ? COLLECTION_JSON_PATH : 'schematics-x';
@@ -38,6 +39,13 @@ export const runSchematic = (schematic: string) => (schematicArgs, options) => {
     .catch((e) => { throw e; });
 };
 
+export const parseBooleanOptions = (_options: object, booleanOptions: string[] = []) => {
+  const parseBoolean = (value) => value === 'true' ? true : value === 'false' ? false : value;
+  return Object.entries(_options).reduce((acc, [key, value]) => {
+    return {...acc, [key]: booleanOptions.includes(key) ? parseBoolean(value) : value};
+  }, {});
+};
+
 export async function main() {
   const program = new Command();
   program.version(packageJson.version, '-v, --version', 'output the current version');
@@ -53,28 +61,31 @@ export async function main() {
       return type === 'boolean' ? `${key} [${type}]` : `${key} <${type}>`;
     };
   
-    Object.entries<any>(properties).forEach(([key, { type, description, default: defaultValue, alias }]) => {
+    Object.entries<any>(properties).forEach(([key, { type, description, default: defaultValue, alias, visible }]) => {
+      if (typeof visible === 'boolean' && !visible) return;
       booleanOptions.push(key);
       command.option(resolveOptionFlags(key, type, alias), description, defaultValue);
     });
   
-    command.option(`--interactive`, `Enable interactive input prompts.`, true);
-    command.option(`--force`, `Force overwriting files that would otherwise be an error.`, false);
-    command.option(`--verbose`, `Show more information.`, false);
-    command.option(`--allow-private`, `Allow private schematics to be run from the command line.`, false);
-    command.option(`--debug`, `Debug mode.`,  null);
-    command.option(`--dry-run`, `Do not output anything, but instead just show what actions would be
+    command.option(`--interactive [boolean]`, `Enable interactive input prompts.`, true);
+    command.option(`--force [boolean]`, `Force overwriting files that would otherwise be an error.`, false);
+    command.option(`--verbose [boolean]`, `Show more information.`, false);
+    command.option(`--allow-private [boolean]`, `Allow private schematics to be run from the command line.`, false);
+    command.option(`--debug [boolean]`, `Debug mode.`,  null);
+    command.option(`--dry-run [boolean]`, `Do not output anything, but instead just show what actions would be
     performed.`, null);
+    command.option(`--in-workspace [boolean]`, 
+      'Run in Angular workspace.(if not exists `angular.json`, automatically disabled.)', true);
 
     command
       .arguments('[args...]')
-      .action((args, _options) => {
-        const parseBoolean = (value) => value === 'true' ? true : value === 'false' ? false : value;
-        const options = Object.entries(_options).reduce((acc, [key, value]) => {
-          return {...acc, [key]: booleanOptions.includes(key) ? parseBoolean(value) : value};
-        }, {});
-        process.env['DEBUG'] && console.debug('options:', options);
-        runSchematic(`${COLLECTION}:${name}`)(args, options);
+      .action(async (args, _options) => {
+        const options = parseBooleanOptions(_options, booleanOptions);
+        const workspace = (options as any).inWorkspace ? await getWorkspace('local') : null;
+        const project = workspace ? getProjectByCwd(workspace) : '';
+        
+        process.env['DEBUG'] && console.debug('options:', {...options, project});
+        runSchematic(`${COLLECTION}:${name}`)(args, {...options, project});
       });
   }
   program.parse();
